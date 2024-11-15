0fc870f0d7423bd1878936a83714af77d27634e9
osamaalsrraj <osamaalsrraj3@gmail.com>
2024-11-15 13:38:49 +0200

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- 
   server: {
-    
     port: 3000,
     proxy: {
       "/api": {
@@ -13,5 +11,6 @@ export default defineConfig({
       },
     },
   },
+  
   plugins: [react()],
 });
