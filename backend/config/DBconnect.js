ef861d23561d257a99501697c1c446d5a894a604
osamaalsrraj <osamaalsrraj3@gmail.com>
2024-11-15 15:20:26 +0200

    updates

--- backend/config/DBconnect.js
+++ backend/config/DBconnect.js
@@ -4,15 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || "";
+let isConnected = false; 
 
 export const DBconnect = async () => {
+  if (isConnected) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+
   try {
     await mongoose.connect(mongoUrl, {
       serverSelectionTimeoutMS: 15000,
       socketTimeoutMS: 45000,
     });
-    console.log('connected');
-
+    isConnected = true;
+    console.log("Connected to MongoDB");
   } catch (err) {
     if (err instanceof Error) {
       console.error("Error connecting to MongoDB:", err.message);
--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,9 @@ import cartRoute from "./Router/cartRoutes.js";
 import orderRoute from "./Router/orderRoutes.js";
 import dashboardRoute from "./Router/dashboardRoutes.js";
 import path from "path";
-
 dotenv.config();
+
+DBconnect();
 const __dirname = path.resolve();
 const PORT = Number(process.env.PORT) || 4000;
 const app = express();
@@ -35,7 +36,6 @@ app.use(cookieParser());
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-DBconnect();
 
 app.get("/test", (req, res) => {
   res.status(200).json({
