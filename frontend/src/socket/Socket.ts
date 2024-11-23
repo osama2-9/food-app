75fee309b10b29a896da4f12a3594b26af0e9034
osamaalsrraj <osamaalsrraj3@gmail.com>
2024-11-23 19:05:11 +0200

    update file ext

--- frontend/src/socket/Socket.ts
+++ frontend/src/socket/Socket.ts
@@ -1,14 +1,11 @@
 import socketIOClient from "socket.io-client";
-const SOCKET_URL = import.meta.env.VITE_SOCKET;
-const clintSocket = socketIOClient(SOCKET_URL, {
-    withCredentials: true,
 
-});
+const SOCKET_URL = import.meta.env.VITE_SOCKET;
+const clintSocket = socketIOClient(SOCKET_URL, { withCredentials: true });
 
 export const Socket = () => {
     clintSocket.on('connect', () => {
         console.log(clintSocket.id);
-
-    })
-    return { clintSocket }
-}
+    });
+    return { clintSocket };
+};
