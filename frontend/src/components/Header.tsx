e0307f4079c9ad0cda5e196c283fb9fa5bc08df0
osamaalsrraj <osamaalsrraj3@gmail.com>
2024-11-23 20:08:10 +0200

    update

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { FaBell } from "react-icons/fa";
-import { Socket } from "../socket/socket"; 
-
+import { Socket } from "../socket/Socket";
 
 interface Update {
   message: string;
