107c76cd067362057ab7a53c8d21ce531db079bf
osamaalsrraj <osamaalsrraj3@gmail.com>
2024-11-23 20:16:20 +0200

    server update

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,6 @@ app.use(
   })
 );
 
-// Middleware setup
 app.use(cookieParser());
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -97,10 +96,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(PORT ,()=>{
-  console.log('SERVER');
-  
-})
+server.listen(PORT, () => {
+  console.log("SERVER");
+});
 
 export default server;
 
