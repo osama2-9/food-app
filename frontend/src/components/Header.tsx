b438769a2bf8d6ac5f44eb49a07e7431e8283277
osamaalsrraj <osamaalsrraj3@gmail.com>
2024-11-23 19:18:53 +0200

    update file extntion

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { FaBell } from "react-icons/fa";
-import { Socket } from "../socket/socket";
+import { Socket } from "../socket/socket.ts";
 
 interface Update {
   message: string;
