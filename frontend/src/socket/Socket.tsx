07988cabc900d011ce3511571f7f0027e50800d8
osamaalsrraj <osamaalsrraj3@gmail.com>
2024-11-24 20:23:08 +0200

    updates to test socket io

--- frontend/src/socket/Socket.tsx
+++ frontend/src/socket/Socket.tsx
@@ -1,6 +1,6 @@
 import { Socket as SocketIOClient, io } from "socket.io-client";
 
-const SOCKET_URL: string = import.meta.env.VITE_SOCKET_DEVELOLMENT;
+const SOCKET_URL: string = import.meta.env.VITE_SOCKET;
 
 const clintSocket: SocketIOClient = io(SOCKET_URL, {
   withCredentials: true,
